12a0272362068907364aa72d920ceb637c523e9a
Arela-Cristina <instagrandine@gmail.com>
2024-12-01 21:10:35 +0100

    import module and add style

--- src/components/body-layout/Body.jsx
+++ src/components/body-layout/Body.jsx
@@ -1,13 +1,18 @@
-import style from "./Body";
+import style from "./Body.module.css";
 import Button from "./ui/button/Button";
 import Card from "./ui/card/Card";
 
 export default function Body() {
   return (
     <>
-      <h1>Body</h1>
-      <Button />
-      <Card />
+      <section className={style.card}>
+        <div className={style.cardButton}>
+          <Button />
+        </div>
+        <div className={style.width}>
+          <Card />
+        </div>
+      </section>
     </>
   );
 }
